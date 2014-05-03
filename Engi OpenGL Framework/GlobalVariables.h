ba82e9b44249ff76cc73ac0bfd29de2ae2a240f4
Engi <christian.kitzmann.gomes@gmail.com>
2014-05-03 03:33:41 -0300

    Moved global variables to this file

---
+++ Engi OpenGL Framework/GlobalVariables.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "Logger.hpp"
+
+extern bool bQuit;                          // Flag to exit the application
+extern Logger logger;                       // Standard log file
